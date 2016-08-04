8c58a97bf37dfd12b3c7cd0678896cb7949cc1a6
dengshunlai <mu3305495@163.com>
2016-08-04 22:23:04 +0800

    Initial Commit

---
+++ DSLNavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSLNavigationController
+//
+//  Created by 邓顺来 on 16/8/4.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
